1fb7068d59a3af26207413583cf686282ea77b55
Vincent Ramdhanie <vramdhanie@gmail.com>
2015-08-30 19:46:39 -0400

    TimeTable function

---
+++ timetables.py
@@ -0,0 +1,13 @@
+"""
+A function that prints the grade-school multiplication table up to 12x12.
+
+This function accepts n and m and prints up to n times tables.
+"""
+
+def tables(n, m):
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            print ("%d x %d = %d\n" % (i, j, i*j))
+        print "\n"
+
+tables(12, 12);
